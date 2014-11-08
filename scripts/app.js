127cc71e666a46dd68403e6b8900c0e4c81c4477
NoamELB <NoamELB@users.noreply.github.com>
2014-11-08 19:40:04 +0200

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+var myApp = angular.module('myApp', [
+	'ngRoute',
+	'siteControllers' // controllers
+]).directive('titleBar', function() { // makes the entire title-bar in another HTML template
+    return {
+	  	restrict: 'E',
+	  	templateUrl: 'partials/title-bar.html'
+    };
+}).config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.
+	when('/', {
+		templateUrl: 'partials/welcome.html',
+		controller: 'welcomeController'
+	}).when('/book1', {
+		templateUrl: 'partials/first-book.html'
+	}).when('/book2', {
+		templateUrl: 'partials/second-book.html',
+		controller: 'book2Controller'
+	}).when('/book3', {
+		templateUrl: 'partials/third-book.html'
+	}).when('/more', {
+		templateUrl: 'partials/more.html'
+	}).when('/about', {
+		templateUrl: 'partials/about.html',
+		controller: 'aboutController'
+	}).otherwise({
+		redirectTo: '/book1' // default page - newest book
+	});
+}]);
+
